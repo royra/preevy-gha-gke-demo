15d47cb4dad930cec26c032523a2da7887728c4b
Roy Razon <roy@livecycle.io>
2024-03-17 21:51:57 +0200

    debug

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -19,7 +19,6 @@ function App() {
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
-        <p>This is a new change</p>
         <a
           className="App-link"
           href="https://reactjs.org"
